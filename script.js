10ea677e4e40d538326bdf802f762012854a5883
sanp1217 <pablosgrad12@gmail.com>
2023-05-30 15:34:52 -0600

    Adds operate function to evaluate general expressions given an operator and adds operators object

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function multiply(a, b){
 function divide(a, b){
     return a / b;
 }
+
+const operators = {
+    '+': (a, b) => add(a, b),
+    '-': (a, b) => substract(a, b),
+    '*': (a, b) => multiply(a, b),
+    '/': (a, b) => divide(a, b)
+};
+
+
+function operate(operator, a, b){
+    if(operator in operators){
+        const operatorFunc = operators[operator];
+        const result = operatorFunc(a, b);
+        return result;
+    }
+    console.log('Invalid operation');
+}
